ecc7024a913a609c15440fe54f9e62934c33e414
Joseane Oliveira <joseaneoliveira.joli@gmail.com>
2022-09-14 21:40:43 -0300

    Requisito 2

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,15 @@
-const fetchProducts = () => {
-  // iniciando o projeto
+const fetchProducts = async (computador) => {
+  if (!computador) {
+    throw new Error('You must provide an url');
+  }
+  try {
+    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${computador}`;
+    const promise = await fetch(url);
+    const result = await promise.json();
+    return result;
+  } catch (error) {
+    return error;
+  }
 };
 
 if (typeof module !== 'undefined') {
